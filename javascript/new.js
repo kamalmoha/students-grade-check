752f04fd568180a9cc049084cba954f793312c69
Kamal Mohamed <kamaalkowdiid@gmail.com>
2020-12-24 14:14:53 +0300

    Add files via upload

---
+++ javascript/new.js
@@ -0,0 +1,23 @@
+const grade='P'
+switch(grade){
+    case 'A' :
+    console.log('congrats you got an A');
+    break;
+    case 'B' :
+    console.log('congrats you got an B');
+    break;
+    case 'C' :
+    console.log('congrats you got an C');
+    break;
+    case 'D' :
+    console.log('congrats you got an D');
+    break;
+    case 'E' :
+    console.log('congrats you got an E');
+    break;
+    case 'F' :
+    console.log('congrats you got an F');
+    break;
+    default:
+    console.log('the grade is not valid');
+} 
